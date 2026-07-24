e4bca590e11a3f25aecd8db4988d0f3caf797ede
Yeison Ipia <yeisonipia48@gmail.com>
2026-07-24 15:07:56 -0500

    Añade tipo de retorno a la función do_run_migrations y mejora la importación de Connection

--- alembicApi/appapi/env.py
+++ alembicApi/appapi/env.py
@@ -2,6 +2,7 @@ import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import pool
+from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import create_async_engine
 from models import Base
 from alembic import context
@@ -44,7 +45,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def do_run_migrations(connection):
+def do_run_migrations(connection: Connection) -> None:
     context.configure(connection=connection, target_metadata=target_metadata)
     with context.begin_transaction():
         context.run_migrations()
